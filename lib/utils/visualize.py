d0c594c666de217c5868c83acd9032ee998d105a
chenyilun95 <chenyilun95@gmail.com>
2018-04-21 21:41:14 +0800

    add visualize

--- lib/utils/visualize.py
+++ lib/utils/visualize.py
@@ -124,14 +124,17 @@ def draw_skeleton(aa, kp, show_skeleton_labels=False):
     'l_hip', 'r_hip', 'l_knee', 'r_knee', 'l_ankle', 'r_ankle']
 
     for i, j in skeleton:
-        if kp[i-1][2] > 0.1 and kp[j-1][2] > 0.1 and kp[i-1][0] >= 0 and kp[i-1][1] >= 0 and kp[j-1][0] >= 0 and kp[j-1][1] >= 0:
+        if kp[i-1][0] >= 0 and kp[i-1][1] >= 0 and kp[j-1][0] >= 0 and kp[j-1][1] >= 0 and \
+            (len(kp[i-1]) <= 2 or (len(kp[i-1]) > 2 and  kp[i-1][2] > 0.1 and kp[j-1][2] > 0.1)):
             cv2.line(aa, tuple(kp[i-1][:2]), tuple(kp[j-1][:2]), (0,255,255), 2)
     for j in range(len(kp)):
         if kp[j][0] >= 0 and kp[j][1] >= 0:
-            if kp[j][2] > 1.1:
-                cv2.circle(aa, tuple(kp[j][:2]), 2, tuple((0,0,255)), 2)
-            elif kp[j][2] > 0.1:
-                cv2.circle(aa, tuple(kp[j][:2]), 2, tuple((255,0,0)), 2)
 
-            if show_skeleton_labels and kp[j][2] > 0.1:
+            if len(kp[j]) > 2:
+                if len(kp[j]) <= 2 or (len(kp[j]) > 2 and kp[j][2] > 1.1):
+                    cv2.circle(aa, tuple(kp[j][:2]), 2, tuple((0,0,255)), 2)
+                elif len(kp[j]) <= 2 or (len(kp[j]) > 2 and kp[j][2] > 0.1):
+                    cv2.circle(aa, tuple(kp[j][:2]), 2, tuple((255,0,0)), 2)
+
+            if show_skeleton_labels and (len(kp[j]) <= 2 or (len(kp[j]) > 2 and kp[j][2] > 0.1)):
                 cv2.putText(aa, kp_names[j], tuple(kp[j][:2]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0))
